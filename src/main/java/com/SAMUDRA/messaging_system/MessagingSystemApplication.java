dfa2ee2fb6a18ec3e5ec9bf2bcd2464180db95f4
samudra <samudragantait74@gmail.com>
2025-10-08 03:51:22 +0530

    Initial commit

---
+++ src/main/java/com/SAMUDRA/messaging_system/MessagingSystemApplication.java
@@ -0,0 +1,17 @@
+package com.SAMUDRA.messaging_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MessagingSystemApplication.class, args);
+
+
+
+	}
+
+}
